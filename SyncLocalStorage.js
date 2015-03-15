7133c561e434861e0d9efe45cb04a1383e30a314
Matthew Forrester <matthew.forrester@speechmarks.com>
2015-03-15 11:27:19 +0000

    Break out buildRe from SyncLocalStorage.js into a seperate module (called FindKeysReBuilder)

--- SyncLocalStorage.js
+++ SyncLocalStorage.js
@@ -1,4 +1,4 @@
-module.exports = (function () {
+module.exports = (function (buildRe) {
 
 "use strict";
 	
@@ -150,39 +150,9 @@ SyncLocalStorage.prototype.findKeys = function(pattern) {
 		i = 0,
 		k = '',
 		starIndex = -1,
-		buildRe = null,
 		r = [],
 		re = null;
 	
-	buildRe = function(pattern) {
-		var mustBeDot = [],
-			j = 0;
-		if (!pattern.match(/^[A-Za-z0-9_\.\*]/)) {
-			return false;
-		}
-		pattern = pattern.replace(/\./g,'\\.');
-        starIndex = pattern.indexOf('*');
-		while (starIndex > -1) {
-			mustBeDot = [starIndex-1,starIndex+1];
-			if (starIndex == pattern.length-1) {
-				mustBeDot.pop();
-			}
-			if (starIndex === 0) {
-				mustBeDot.shift();
-			}
-			for (j = 0; j < mustBeDot.length; j++) {
-				if (
-					(pattern.substr(mustBeDot[j],1) !== '.') && 
-					(pattern.substr(mustBeDot[j],1) !== '\\')
-				) {
-					return false;
-				}
-			}
-			pattern = pattern.replace('*','[a-z0-9A-Z_\\-]+');
-			starIndex = pattern.indexOf('*');
-		}
-		return new RegExp('^'+pattern+'$');
-	};
 	
 	re = buildRe(pattern);
 
@@ -210,4 +180,4 @@ SyncLocalStorage.prototype.findKeys = function(pattern) {
 
 return SyncLocalStorage;
 
-}());
+}(require('./FindKeysReBuilder.js')));
